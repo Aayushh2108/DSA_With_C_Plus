46d9b4f6b3b9be7a67c269aaebc86199e660ece0
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2024-04-07 02:00:45 +0530

    Add files via upload

---
+++ Conditional/lec_03.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cin>> a >> b;
+
+    // if(a>0){
+    //     cout<< "A is Positive" << endl;
+
+    // }else{
+    //     cout<< "A is Negative"<< endl;
+    // }
+    cout<< "Value of a and b is "<< a << " "<< b << endl;
+}
